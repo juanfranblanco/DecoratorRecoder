4b3f0d3aa825a3a2c4d556c93049dc9b57c370b3
Juan Blanco <juanfranblanco@hotmail.com>
2015-12-30 07:04:20 +0000

    Initial

---
+++ MethodInterceptorRecorder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorRecorder
+{
+    //using RealProxy as described here http://stackoverflow.com/a/32238417
+    //and here http://www.codeproject.com/Articles/229486/Generic-Dynamic-Decorator-modified
+    //and here http://simpleinjector.readthedocs.org/en/latest/InterceptionExtensions.html
+
+    public class MethodInterceptorRecorder<T> : RealProxy where T: class
+    {
+
+        private T _decorated;
+        public List<RecordedMethod<T>> RecordedMethods { get; private set; }
+
+        public MethodInterceptorRecorder(T decorated)
+            : base(typeof(T))
+        {
+            _decorated = decorated;
+            RecordedMethods = new List<RecordedMethod<T>>();
+        }
+
+        public override IMessage Invoke(IMessage msg)
+        {
+            var methodCall = msg as IMethodCallMessage;
+            var methodInfo = methodCall.MethodBase;
+            //pre call   
+            var recordedMethod = new RecordedMethod<T>() { MethodBase = methodInfo, Arguments = methodCall.InArgs };
+            var result = recordedMethod.ExecuteMethod(_decorated);
+            //post call
+            RecordedMethods.Add(recordedMethod);
+        
+            return new ReturnMessage(result, null, 0,
+                methodCall.LogicalCallContext, methodCall);
+        }
+
+        public void ReplayMethods(T obj)
+        {
+            foreach(var method in RecordedMethods)
+            {
+                method.ExecuteMethod(obj);
+            }
+        }
+
+    }
+
+
+    public class RecordedMethod<T>
+    {
+        public MethodBase MethodBase { get; set; }
+        public object[] Arguments { get; set; }
+
+        public object ExecuteMethod(T obj)
+        {
+            return MethodBase.Invoke(obj, Arguments);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorRecorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var innerRobot = new Robot();
+            var recorder = new MethodInterceptorRecorder<IRobot>(innerRobot);
+            IRobot robot = (IRobot)recorder.GetTransparentProxy();
+
+            robot.Fire(10);
+            robot.Jump(1);
+            robot.Move(5, 6);
+
+            Console.WriteLine("Playing recorded");
+            recorder.ReplayMethods(innerRobot);
+
+            Console.ReadLine();
+
+            
+        }
+
+        public class Robot : IRobot
+        {
+            public void Fire(int numberOfTimes)
+            {
+                Console.WriteLine("Fired " + numberOfTimes);
+            }
+
+            public void Jump(int height)
+            {
+                Console.WriteLine("Jumped " + height);
+            }
+
+            public void Move(int x, int y)
+            {
+                Console.WriteLine("Moved x:" + x + " y" + y);
+            }
+        }
+
+        public interface IRobot
+        {
+            void Move(int x, int y);
+            void Jump(int height);
+            void Fire(int numberOfTimes);
+        }
+    }
+}
